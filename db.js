f3634ae5a78b168b81ecce3de4ad125ed79d4e38
ydrea <ydrea.wrld@gmail.com>
2023-08-16 12:36:19 +0200

    cpanel

--- db.js
+++ db.js
@@ -1,12 +1,6 @@
 const path = require('path');
 const ssl = require('ssl');
-require('dotenv')
-  .config
-  //   {
-  //   override: true,
-  //   path: path.join(__dirname, 'g.env'),
-  // }
-  ();
+require('dotenv').config();
 const pg = require('pg');
 
 const pool = new pg.Pool({
@@ -21,8 +15,8 @@ const trip = async () => {
   const client = await pool.connect();
   try {
     const { rows } = await client.query('SELECT current_user');
-    const uSer = rows[0]['current_user'];
-    console.log(uSer);
+    const uer = rows[0]['current_user'];
+    console.log(user);
   } catch (err) {
     console.error(err);
   } finally {
@@ -31,43 +25,3 @@ const trip = async () => {
 };
 trip();
 module.exports = pool;
-// {
-//   query: (text, params) => pool.query(text, params),
-// };
-
-// const path = require('path');
-// require('dotenv').config({
-//   override: true,
-//   path: path.join(__dirname, 'g.env'),
-// });
-// const pg = require('pg');
-
-// const pool = new pg.Pool({
-// user: 'banija_www',
-// host: 'landscape.agr.hr',
-// database: 'banija',
-// password: '1akauntzabanijawwwpotrebe2',
-// port: 5434,
-// requiressl: true, // { rejectUnauthorized: false },
-//   //
-//   connectionTimeoutMillis: 22222,
-//   idleTimeoutMillis: 11111,
-//   allowExitOnIdle: false,
-//   //
-// });
-// // const connectDb =
-// async () => {
-//   const client = await pool.connect();
-//   try {
-//     const { rows } = await client.query('SELECT current_user');
-//     const uSer = rows[0]['current_user'];
-//     console.log(uSer);
-//   } catch (err) {
-//     console.error(err);
-//   } finally {
-//     client.release();
-//   }
-// };
-// module.exports = {
-//   query: (text, params) => pool.query(text, params),
-// };
